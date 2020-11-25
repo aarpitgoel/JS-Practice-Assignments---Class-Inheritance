edfc57a9c1ec0beae041ff71e4a2db22ad5d5033
aarpitgoel <64373216+aarpitgoel@users.noreply.github.com>
2020-11-25 06:42:07 +0530

    update

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,10 @@
-// import using require
-
 // declare class
-
-
-// export class using module.exports
+const Shape = require("./shape.js");
+class Circle extends Shape {
+  constructor() {
+    super();
+    this.color = "red";
+  }
+  calculateArea() {}
+}
+module.exports = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,11 @@
 // declare class
 
-// export class using module.exports
+class Shape{
+  constructor(){
+    this.color="blue";
+  }
+  drawShape(){}
+  calculateArea(){}
+};
+
+module.exports=Shape
